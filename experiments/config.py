dff0eeda3424db1a7674fd4821fa1841a4a6d8ab
Aru <rm.arumugam.2000@gmail.com>
2021-11-12 16:53:28 +0530

    Remove Experiment config
    Experiment config was removed and replaced by a nested dictionary
    structure that could be loaded from a YAML or a json file

--- experiments/config.py
+++ experiments/config.py
@@ -1,16 +1,26 @@
-import typing as T
-from dataclasses import dataclass
-
-
-@dataclass(frozen=True, eq=False)
-class ExperimentConfig:
-
-    experiment_name: str
-    network_arch: T.List[int]
-    activation_function: T.Any
-    num_training_envs: int
-    timesteps: int
-    batch_size: int = 64
-    train_episode_length: int = 25
-    test_episode_length: int = 100
-    n_steps: int = 25
+env = "Placeholder"
+eval_env = "Placeholder"
+ReLU = "Placeholder"
+example_train_config = dict(
+    experiment_name = "ppo_simple",
+    ppo_kwargs = dict(
+        policy = "MlpPolicy",
+        env=env,
+        batch_size=64,
+        verbose=1,
+        policy_kwargs = dict(
+            net_arch = [16,12,8],
+            activation_fn=ReLU,
+        ),
+        n_epochs=4,
+        n_steps=25
+    ),
+    eval_callback_kwargs = dict(
+        eval_env=eval_env,
+        deterministic=True,
+        render=False,
+        n_eval_episodes=100,
+        eval_freq=(10000//16)
+    ),
+    timesteps = 100_000
+)
--- experiments/ppo.py
+++ experiments/ppo.py
@@ -6,15 +6,11 @@ import typing as T
 import torch as th
 import gym
 
-from torch import nn
-
 from datetime import datetime
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.common.policies import ActorCriticPolicy
 from supersuit.vector.sb3_vector_wrapper import SB3VecEnvWrapper
-from torch.nn import ReLU
-from experiments.config import ExperimentConfig
 
 from experiments.utils import wrap_parallel_env
 
@@ -22,8 +18,8 @@ from experiments.utils import wrap_parallel_env
 class CustomPolicy(ActorCriticPolicy):
     def __init__(
         self,
-        observation_space: gym.spaces.Space,
-        action_space: gym.spaces.Space,
+        observation_space: gym.Space,
+        action_space: gym.Space,
         lr_schedule,
         num_agents=1,
         **kwargs,
@@ -38,84 +34,91 @@ class CustomPolicy(ActorCriticPolicy):
         landmarks = obs[:, 4 : self.num_agents * 2 + 4]
         landmarks = landmarks.reshape((obs.shape[0], self.num_agents, 2))
 
-        for ind, landmark in enumerate(landmarks):
+        for ind in range(len(landmarks)):
             perm = th.randperm(self.num_agents)
             landmarks[ind, :] = landmarks[ind, perm, :]
 
-
         shuffled_landmarks = landmarks.reshape((obs.shape[0], self.num_agents * 2))
-        obs = th.cat([obs[:, :4], shuffled_landmarks, obs[:, self.num_agents * 2 + 4 :]], dim=-1)
+        obs = th.cat(
+            [obs[:, :4], shuffled_landmarks, obs[:, self.num_agents * 2 + 4 :]], dim=-1
+        )
         return super().forward(obs, deterministic=deterministic)
 
 
 def create_simple_envs(
-    config: ExperimentConfig,
+    train_episode_length, test_episode_length, num_training_envs
 ) -> T.Tuple[SB3VecEnvWrapper, SB3VecEnvWrapper]:
-    env = simple_v2.parallel_env(max_cycles=config.train_episode_length)
-    env = wrap_parallel_env(env, num_envs=config.num_training_envs)
+    env = simple_v2.parallel_env(max_cycles=train_episode_length)
+    env = wrap_parallel_env(env, num_envs=num_training_envs)
 
-    eval_env = simple_v2.parallel_env(max_cycles=config.test_episode_length)
+    eval_env = simple_v2.parallel_env(max_cycles=test_episode_length)
     eval_env = wrap_parallel_env(eval_env, num_envs=1)
     return env, eval_env
 
 
 def create_simple_spread_envs(
-    config: ExperimentConfig, N=1
+    train_episode_length, test_episode_length, num_training_envs, N=1
 ) -> T.Tuple[SB3VecEnvWrapper, SB3VecEnvWrapper]:
-    env = simple_spread_v2.parallel_env(N=N, max_cycles=config.train_episode_length)
-    env = wrap_parallel_env(env, num_envs=config.num_training_envs)
+    """Create and wrap a simple spread environment.
 
-    eval_env = simple_spread_v2.parallel_env(N=N, max_cycles=config.test_episode_length)
+    Args:
+        N: number of agents in the simple spread env
+    """
+    env = simple_spread_v2.parallel_env(N=N, max_cycles=train_episode_length)
+    env = wrap_parallel_env(env, num_envs=num_training_envs)
+
+    eval_env = simple_spread_v2.parallel_env(N=N, max_cycles=test_episode_length)
     eval_env = wrap_parallel_env(eval_env, num_envs=1)
     return env, eval_env
 
 
-def train(env: SB3VecEnvWrapper, eval_env: SB3VecEnvWrapper, config: ExperimentConfig):
+def train(
+    experiment_name: str,
+    ppo_kwargs: T.Dict[str, T.Any],
+    eval_callback_kwargs: T.Dict[str, T.Any],
+    timesteps: int,
+):
+    """Train a given configuratio
 
-    dir_name = (
-        f"{config.experiment_name}-{datetime.now().strftime('%d-%m-%yT%H-%M-%S')}"
-    )
-    model_save_path = os.path.join(dir_name, config.experiment_name)
+    Args:
+        experiment_name: name of the experiment
+        ppo_kwargs: kwarg dictionary to be passed to the PPO algorithm
+        eval_callback_kwargs: kwarg dictionary to be passed to the eval callback
+        timestep: number of timesteps to train the algorithm for
+    """
+
+    # assign names of directories to use for saving
+    dir_name = f"{experiment_name}-{datetime.now().strftime('%d-%m-%yT%H-%M-%S')}"
+    model_save_path = os.path.join(dir_name, experiment_name)
     log_dir = os.path.join(dir_name, "logs/")
-
     pathlib.Path(dir_name).mkdir(exist_ok=True)
-
-    # Parallel environments
-
-    eval_freq = 10_000
-    eval_freq = max(eval_freq // config.num_training_envs, 1)
-
-    eval_cb = EvalCallback(
-        eval_env,
+    eval_callback_kwargs.update(
         best_model_save_path=log_dir,
         log_path=log_dir,
-        deterministic=True,
-        render=False,
-        eval_freq=eval_freq,
-        n_eval_episodes=100,
     )
 
-    print(f"Observation space: {env.observation_space}")
-    print(f"Number of environments: {env.num_envs}")
+    # create model and callback
+    model = PPO(**ppo_kwargs)
+    eval_cb = EvalCallback(**eval_callback_kwargs)
 
-    model = PPO(
-        CustomPolicy,
-        env,
-        batch_size=config.batch_size,
-        n_steps=config.n_steps,
-        verbose=1,
-        policy_kwargs=dict(
-            net_arch=config.network_arch,
-            activation_fn=config.activation_function,
-            num_agents=2,
-        ),
-        n_epochs=4
-    )
-    model.learn(total_timesteps=config.timesteps, callback=eval_cb)
+    # train the model
+    model.learn(total_timesteps=timesteps, callback=eval_cb)
 
+    # save the trained model
     model.save(model_save_path)
+
+    # save the config
     with open(model_save_path, "w") as f:
-        f.write(repr(config) + "\n")
+        f.write(
+            "\n".join(
+                [
+                    "Experiment name: " + experiment_name,
+                    "Model key word arguments: " + repr(ppo_kwargs),
+                    "Eval callback key word arguments: " + repr(eval_callback_kwargs),
+                    "Timesteps: " + repr(timesteps),
+                ]
+            )
+        )
 
     return model, model_save_path
 
@@ -123,20 +126,18 @@ def train(env: SB3VecEnvWrapper, eval_env: SB3VecEnvWrapper, config: ExperimentC
 def test(
     env: SB3VecEnvWrapper,
     model: PPO,
-    config: ExperimentConfig,
-    model_save_path: T.Optional[str] = None,
     render=False,
-    N=200,
+    num_episodes=200,
 ):
 
     all_rewards = []
-    for ep_id in range(N):
+    for _ in range(num_episodes):
 
         total_reward = 0
         obs = env.reset()
         while True:
-            action, _states = model.predict(obs, deterministic=True)
-            obs, rewards, dones, info = env.step(action)
+            action, _ = model.predict(obs, deterministic=True)
+            obs, rewards, dones, _ = env.step(action)
 
             if render:
                 env.venv.vec_envs[0].par_env.unwrapped.render()
@@ -147,10 +148,5 @@ def test(
                 break
 
         all_rewards.append(total_reward)
-
-    if model_save_path is not None:
-        with open(model_save_path, "a") as f:
-            f.write(repr(all_rewards) + "\n")
-            f.write(repr(sum(all_rewards) / len(all_rewards)) + "\n")
 
     return all_rewards
